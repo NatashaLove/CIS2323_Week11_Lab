758ba413753b6d717b6fb6ae42102c4f1b859d72
Natasha S Love <debora@inbox.ru>
2018-10-31 18:58:14 -0500

    a comment

--- app/src/main/java/com/example/debor/todolist/MainActivity.java
+++ app/src/main/java/com/example/debor/todolist/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.debor.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//just a comment
 public class MainActivity extends AppCompatActivity {
 
     @Override
