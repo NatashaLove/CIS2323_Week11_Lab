493a392b5bee82929057a3375c63581b59e6f9a8
Natasha S Love <debora@inbox.ru>
2018-10-31 19:09:09 -0500

    added comment

--- app/src/main/java/com/example/debor/todolist/MainActivity.java
+++ app/src/main/java/com/example/debor/todolist/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //just a comment
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onDestroy() {
+        //do smth else
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
